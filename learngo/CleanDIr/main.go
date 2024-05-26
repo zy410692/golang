6a3d1f26cad732d29e5839c5ffb5c8539953a3c4
zy410692 <yizhang890320@163.com>
2024-05-26 09:34:56 +0800

    bug

--- learngo/CleanDIr/main.go
+++ learngo/CleanDIr/main.go
@@ -13,7 +13,7 @@ const (
 	dirPath = "/data/minio-data/yunwei/backup/mysql/10_5_0_126"
 )
 
-func cleandir() {
+func cleanfile() {
 	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -21,29 +21,14 @@ func cleandir() {
 		log.Println(path)
 		log.Println(info)
 		log.Println(info.ModTime())
+
 		if !info.IsDir() && time.Since(info.ModTime()) > 100*24*time.Hour {
 			err := os.Remove(path)
 			if err != nil {
 				return err
 			}
 		}
-		if info.IsDir() && time.Since(info.ModTime()) > 100*24*time.Hour {
-			files, err := ioutil.ReadDir(dirPath + "/" + info.Name())
-			if err != nil {
-				log.Fatalf("Failed to read directory: %v", err)
-			}
-
-			if len(files) == 0 {
-				err := os.Remove(dirPath + "/" + info.Name())
-				if err != nil {
-					log.Fatalf("Failed to remove directory: %v", err)
-				} else {
-					log.Printf("Directory %v removed successfully", info.Name())
-				}
-			} else {
-				log.Printf("Directory %v is not empty", info.Name())
-			}
-		}
+		go cleandir(path)
 
 		return nil
 	})
@@ -53,12 +38,32 @@ func cleandir() {
 	}
 }
 
+func cleandir(file string) {
+	dir := filepath.Dir(file)
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatalf("Failed to read directory: %v", err)
+	}
+
+	if len(files) == 0 {
+		err := os.Remove(dir)
+		if err != nil {
+			log.Fatalf("Failed to remove directory: %v", err)
+		} else {
+			log.Printf("Directory %v removed successfully", dir)
+		}
+	} else {
+		log.Printf("Directory %v is not empty", dir)
+	}
+
+}
+
 func main() {
 	ticker := time.NewTicker(10 * time.Second)
 
 	go func() {
 		for t := range ticker.C {
-			cleandir()
+			cleanfile()
 			fmt.Println("Task runs at:", t)
 		}
 	}()
